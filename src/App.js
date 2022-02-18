5c378fe87191c6c7f7beb5eded104ac16a8c8c34
superlgtele <superlgtele@naver.com>
2022-02-18 17:10:34 +0900

    ReStart
    2022.02.18

--- src/App.js
+++ src/App.js
@@ -1,62 +1,30 @@
 /* eslint-disable */
-
 import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [title, changetitle] = useState(["마포갈매기", "명륜지사갈비"]);
-  let [good, changegood] = useState(0);
-  let [modalonoff, changemodal] = useState(false);
+  let [title, changetitle] = useState(["강남우동맛집", "일산마포갈매기"]);
 
-  function 제목바꾸는함수() {
+  function copy() {
     let newArray = [...title];
-    newArray[1] = "성공!";
+    newArray[0] = "일본우동맛집";
     changetitle(newArray);
-  }
-
-  function 모달온오프함수() {
-    if (modalonoff == false) {
-      changemodal(true);
-    } else {
-      changemodal(false);
-    }
   }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발시작</div>
+        <div>Blog</div>
       </div>
-      <button onClick={제목바꾸는함수}>제목바꾸기</button>
-      <button onClick={모달온오프함수}>모달창온오프하기</button>
+      <button onClick={copy}>제목바꾸기</button>
       <div className="list">
-        <h3>
-          {title[1]} <span onClick={() => changegood(good + 1)}>👍</span> {good}
-        </h3>
-        <p>갈매기살 전문점</p>
-        <hr />
+        <h3>{title[0]}</h3>
+        <p>2월 18일 발행</p>
       </div>
-
-      {title.map(function (titledata) {
-        return (
-          <div className="list">
-            <h3>{titledata}</h3>
-            <hr />
-          </div>
-        );
-      })}
-
-      {modalonoff == true ? <Modal 작명title={title} /> : null}
-    </div>
-  );
-}
-
-function Modal(props) {
-  return (
-    <div className="modal">
-      <h2>{props.작명title[0]}</h2>
-      <p>날짜</p>
-      <p>상세내용</p>
+      <div className="list">
+        <h3>{title[1]}</h3>
+        <p>2월 20일 발행</p>
+      </div>
     </div>
   );
 }
