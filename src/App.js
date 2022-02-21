a30f95ed93fa9ed938683f2fcf0d13627737362f
superlgtele <superlgtele@naver.com>
2022-02-21 16:00:33 +0900

    Props
    2022/02/21

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 /* eslint-disable */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import ShoesData from "./data";
 
 function App() {
   let [title, changetitle] = useState(["강남우동맛집", "일산마포갈매기"]);
+  let [shoes, changeshoes] = useState(ShoesData);
 
   function copy() {
     let newArray = [...title];
-    newArray[0] = "일본우동맛집";
+    newArray[0] = "일본초밥맛집";
     changetitle(newArray);
   }
 
@@ -20,11 +22,38 @@ function App() {
       <div className="list">
         <h3>{title[0]}</h3>
         <p>2월 18일 발행</p>
+        <hr />
       </div>
       <div className="list">
         <h3>{title[1]}</h3>
         <p>2월 20일 발행</p>
+        <hr />
       </div>
+      {shoes.map((a, i) => {
+        return <Card shoes={a} i={i} key={i} />;
+      })}
+    </div>
+  );
+}
+
+function Card(props) {
+  // useEffect(() => {
+  //   if (shoes) {
+  //     console.log(shoes);
+  //   }
+  // });
+
+  return (
+    <div className="list">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="20%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}원</p>
+      <hr />
     </div>
   );
 }
